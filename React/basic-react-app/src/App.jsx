cb048a1b6fc06613ae3a64656e367960a0253934
yogeshrana007 <ranayogesh794@gmail.com>
2025-03-20 09:21:31 +0530

    feat: Integrate ProductTab components in App

---
+++ React/basic-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Activity from "./Activity.jsx";
+import "./App.css";
+import ProductTab from "./ProductTab.jsx";
+function App() {
+    return (
+        <>
+            <Activity username="Yogesh" textColor="pink" />
+            <ProductTab />
+        </>
+    );
+}
+// Update the import statement to match the correct file and function name
+export default App;
