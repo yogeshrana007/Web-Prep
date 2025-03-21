bdfa94c00fec240aa5278c3351b2b9b101dae2ad
yogeshrana007 <ranayogesh794@gmail.com>
2025-03-21 22:14:34 +0530

    Add Product component to display title, description, and pricing details

--- React/basic-react-app/src/Product.jsx
+++ React/basic-react-app/src/Product.jsx
@@ -1,21 +1,21 @@
+import Price from "./Price.jsx";
 import "./Product.css";
 
-function Product({ title, price = 50, features = [] }) {
-    // Here price=50 means we are giving default value of price if none is assign the price=50.
-
-    let styles = {
-        backgroundColor: price > 30000 ? "lightgreen" : "lightblue",
-    };
+function Product({ title, idx }) {
+    let oldPrice = [12000, 10000, 50000, 20000];
+    let newPrice = [9999, 8999, 45999, 18999];
+    let description = [
+        ["8,000 DPI sensor", "wireless"],
+        ["intuitive touch surface", "wireless"],
+        ["designed for iPad Pro", "wireless"],
+        ["wireless mouse", "wireless"],
+    ];
     return (
-        <div className="Product" style={styles}>
+        <div className="Product">
             <h3>{title}</h3>
-            <h5>price is {price} </h5>
-            {features.map((feature) => (
-                <p>
-                    <li>{feature}</li>
-                </p>
-            ))}
-            {price > 30000 && <p>Discount of 5%</p>}
+            <p>{description[idx][0]}</p>
+            <p>{description[idx][1]}</p>
+            <Price oldPrice={oldPrice[idx]} newPrice={newPrice[idx]} />
         </div>
     );
 }
