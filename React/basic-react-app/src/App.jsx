0803df26906adc5057c440ab94ad806929a6e4fb
yogeshrana007 <ranayogesh794@gmail.com>
2025-03-21 22:17:18 +0530

    Update App component to render Activity, ProductTab, Button, and Form components

--- React/basic-react-app/src/App.jsx
+++ React/basic-react-app/src/App.jsx
@@ -1,11 +1,16 @@
 import Activity from "./Activity.jsx";
 import "./App.css";
+import Button from "./Button.jsx";
+import Form from "./Form.jsx";
 import ProductTab from "./ProductTab.jsx";
 function App() {
     return (
         <>
             <Activity username="Yogesh" textColor="pink" />
+            <h1>Top deals</h1>
             <ProductTab />
+            <Button />
+            <Form />
         </>
     );
 }
