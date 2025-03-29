4474877880a3c5ab7d5c12e61a634b58972c32bf
yogeshrana007 <ranayogesh794@gmail.com>
2025-03-30 01:28:54 +0530

    Refactor LotteryGame component to separate ticket generation logic and improve structure with props for dynamic ticket size and winning condition

--- React/react-state-class/src/LotteryGame.jsx
+++ React/react-state-class/src/LotteryGame.jsx
@@ -1,3 +1,4 @@
+/*
 import { useState } from "react";
 
 function genRandomNum(n) {
@@ -7,9 +8,7 @@ function genRandomNum(n) {
     }
     return arr;
 }
-function sum(arr) {
-    return arr.reduce((sum, curr) => sum + curr, 0);
-}
+
 
 export default function LotteryGame() {
     let [ticket, setTicket] = useState(genRandomNum(3));
@@ -33,3 +32,38 @@ export default function LotteryGame() {
 }
 
 // Here we made all components in one file. Generally, we shouldn't do this. Instead, we should create separate files for each component.
+*/
+
+import { useState } from "react";
+import Ticket from "./Ticket.jsx";
+
+// we have to make another file for this getRandomNum
+function genRandomNum(n) {
+    let arr = new Array(n);
+    for (let i = 0; i < n; i++) {
+        arr[i] = Math.floor(Math.random() * 10);
+    }
+    return arr;
+}
+
+function sum(arr) {
+    return arr.reduce((sum, curr) => sum + curr, 0);
+}
+
+// below is the that shows how to code the problem and make generalized form of any problem
+export default function LotteryGame({ n, winningSum }) {
+    let [ticket, setTicket] = useState(genRandomNum(n));
+    let isWinning = sum(ticket) === winningSum;
+
+    let buyTicket = () => {
+        setTicket(genRandomNum(n));
+    };
+    return (
+        <div>
+            <h1>Lottery</h1>
+            <Ticket ticket={ticket} />
+            <button onClick={buyTicket}>Buy new ticket</button>
+            <h3>{isWinning && "Congrats you won!!"}</h3>
+        </div>
+    );
+}
