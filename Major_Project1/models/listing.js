9b120900c338beb96b096e20353fe976a4c2c49c
Yogesh Rana <ranayogesh794@gmail.com>
2025-06-01 13:39:29 +0530

    Add Listing model schema with fields for title, description, image, price, location, and country

---
+++ Major_Project1/models/listing.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        filename: {
+            type: String,
+            default: "listingimage",
+        },
+        url: {
+            type: String,
+            default:
+                "https://plus.unsplash.com/premium_photo-1661964071015-d97428970584?q=80&w=1920&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        },
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    location: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+module.exports = Listing;
