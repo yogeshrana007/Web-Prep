6dc4123997032fe4ebab1ab23971c41065b83096
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-01 10:24:18 +0530

    Add customer and user models with initial data insertion logic

---
+++ Relationships/Models/customer.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+main()
+    .then(() => console.log("connection successful"))
+    .catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1:27017/relationDemo");
+}
+
+const orderSchema = new Schema({
+    item: String,
+    price: Number,
+});
+
+const customerSchema = new Schema({
+    name: String,
+    orders: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Order",
+        },
+    ],
+});
+
+const Order = mongoose.model("Order", orderSchema);
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async () => {
+    let cust1 = new Customer({
+        name: "Rahul",
+    });
+
+    let order1 = await Order.findOne({ item: "chips" });
+    let order2 = await Order.findOne({ item: "chocolate" });
+
+    cust1.orders.push(order1);
+    cust1.orders.push(order2);
+
+    let res = await cust1.save();
+    console.log(res);
+};
+addCustomer();
+// const addOrders = async () => {
+//     let result = await Order.insertMany([
+//         { item: "Samosa", price: 12 },
+//         { item: "chips", price: 10 },
+//         { item: "chocolate", price: 40 },
+//     ]);
+//     console.log(result);
+// };
+
+// addOrders();
