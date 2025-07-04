6d9977153fcb2ead9ad0d5ea194d2a7b77c18702
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-04 23:46:05 +0530

    Add token generation utility using jsonwebtoken

---
+++ LinkedIn_Clone/Backend/config/token.js
@@ -0,0 +1,12 @@
+import jwt from "jsonwebtoken";
+
+const genToken = async (userId) => {
+    try{
+        let token = await jwt.sign({userId},process.env.JWT_SECRETE,{expiresIn:"7d"})
+        return token;
+    } catch(error){
+        console.log(error);
+    }
+}
+
+export default genToken;
