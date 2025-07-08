405af4513ac7f6285673caaf4fbd56f58ba99abd
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-08 10:21:00 +0530

    Authentication middleware to verify user tokens

---
+++ LinkedIn_Clone/Backend/middlewares/isAuth.js
@@ -0,0 +1,23 @@
+import jwt from "jsonwebtoken";
+
+const isAuth = async (req, res, next) => {
+    try {
+        const { token } = req.cookies;
+        if (!token) {
+            return res.status(400).json({ message: "user doesn't have token" });
+        }
+        const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verifyToken) {
+            return res
+                .status(400)
+                .json({ message: "user doesn't have valid token" });
+        }
+        console.log(verifyToken);
+        req.userId = verifyToken.userId;
+        next();
+    } catch (error) {
+        return res.status(500).json({ message: "Invalid or expired token" });
+    }
+};
+
+export default isAuth;
