c0a5fca965e7374dd9ddba05a2f6d99bfaf232ae
Yogesh Rana <ranayogesh794@gmail.com>
2025-07-17 19:33:47 +0530

    Fix formatting and correct typo in JWT secret variable in token generation function

--- LinkedIn_Clone/Backend/config/token.js
+++ LinkedIn_Clone/Backend/config/token.js
@@ -1,12 +1,14 @@
 import jwt from "jsonwebtoken";
 
 const genToken = async (userId) => {
-    try{
-        let token = await jwt.sign({userId},process.env.JWT_SECRETE,{expiresIn:"7d"})
+    try {
+        let token = await jwt.sign({ userId }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
         return token;
-    } catch(error){
+    } catch (error) {
         console.log(error);
     }
-}
+};
 
 export default genToken;
