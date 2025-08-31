d6bda79872c58e0908ac13cab85effe62d3e078b
Yogesh Rana <ranayogesh794@gmail.com>
2025-08-31 09:45:16 +0530

    Fix casing for SameSite attribute in login cookie settings

--- LinkedIn_Clone/Backend/controllers/auth.controllers.js
+++ LinkedIn_Clone/Backend/controllers/auth.controllers.js
@@ -65,7 +65,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, {
             httpOnly: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
-            sameSite: "strict",
+            sameSite: "Strict",
             secure: process.env.NODE_ENVIRONMENT === "production",
         });
         return res.status(200).json(user);
