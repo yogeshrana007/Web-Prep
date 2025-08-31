3ac0388b50e6af4914d265e3e5a1e40e89eb865b
Yogesh Rana <ranayogesh794@gmail.com>
2025-08-31 09:51:48 +0530

    User routes for getCurrent user, update profile.

---
+++ LinkedIn_Clone/Backend/routes/user.routes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import {
+    getCurrentUser,
+    updateProfile,
+} from "../controllers/user.controllers.js";
+import isAuth from "../middlewares/isAuth.js";
+import upload from "../middlewares/multer.js";
+
+let userRouter = express.Router();
+
+userRouter.get("/currentuser", isAuth, getCurrentUser);
+userRouter.put(
+    "/updateprofile",
+    isAuth,
+    upload.fields([
+        { name: "profileImage", maxCount: 1 },
+        { name: "coverImage", maxCount: 1 },
+    ]),
+    updateProfile
+);
+
+export default userRouter;
