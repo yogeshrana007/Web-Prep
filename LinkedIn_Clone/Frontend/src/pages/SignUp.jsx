411937e99e0140c04ca675c344a048662e021770
Yogesh Rana <ranayogesh794@gmail.com>
2025-08-31 09:53:36 +0530

    Signup component for user can create account

--- LinkedIn_Clone/Frontend/src/pages/SignUp.jsx
+++ LinkedIn_Clone/Frontend/src/pages/SignUp.jsx
@@ -2,15 +2,18 @@ import axios from "axios";
 import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import logo from "../assets/logo.svg";
-import { authDataContext } from "../context/AuthContext.jsx";
+import { AuthDataContext } from "../context/AuthContext.jsx";
+import { UserDataContext } from "../context/UserContext.jsx";
+
 import "../styles/style.css";
 
 function SignUp() {
     let [show, setshow] = useState(false);
     let navigate = useNavigate();
-    let { serverUrl } = useContext(authDataContext);
+    const { serverUrl } = useContext(AuthDataContext);
     let [loading, setLoading] = useState(false);
     let [err, setErr] = useState("");
+    let { userData, setUserData, getCurrentUser } = useContext(UserDataContext);
 
     const [formData, setFormData] = useState({
         firstName: "",
@@ -38,19 +41,27 @@ function SignUp() {
                 formData,
                 { withCredentials: true }
             );
-            setFormData({
-                firstName: "",
-                lastName: "",
-                userName: "",
-                email: "",
-                password: "",
-            });
+            setUserData(result.data);
+            {
+                if (result.status === 201) {
+                    await getCurrentUser();
+                    setFormData({
+                        firstName: "",
+                        lastName: "",
+                        userName: "",
+                        email: "",
+                        password: "",
+                    });
+                    navigate("/");
+                }
+            }
+
             setLoading(false);
         } catch (error) {
             setLoading(false);
 
             // console.log("Signup error response:", error.response?.data);
-            setErr(error.response.data.message);
+            setErr(error?.response?.data?.message || "Something went wrong");
         }
     };
 
@@ -128,7 +139,7 @@ function SignUp() {
                     {err && <p className="text-red-600">{err}</p>}
                     <button
                         disabled={loading}
-                        className="w-full md:w-auto bg-[#24b2ff] text-white font-semibold mt-8 py-3 px-4 rounded-full hover:bg-blue-700 flex itmes-center justify-center "
+                        className="w-full md:w-auto bg-[#24b2ff] text-white font-semibold mt-8 py-3 px-4 rounded-full hover:bg-blue-700 flex items-center justify-center "
                     >
                         {loading ? "Loading.." : "Sign Up"}
                     </button>
